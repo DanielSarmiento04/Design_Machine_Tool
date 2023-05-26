f8be3f712b2ba2cee19da0fdf670e939d7c40a0e
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-05-25 22:57:37 -0500

    add spring concept

--- utils/__init__.py
+++ utils/__init__.py
@@ -37,3 +37,40 @@ def calculate_k_f(d, units='usa')-> float:
         return 5.7 + 0.6812 * d
     
     return 5.7 + 0.02682 * d
+
+import numpy as np
+
+def calculate_indice_resorte(D:float, d:float):
+    '''
+        Description
+        -----------
+        This function is used to calculate the indice resorte
+    '''
+    return D / d
+
+
+def calcula_k_s(c):
+    '''
+        Description
+        -----------
+        This function is used to calculate the k_s
+    '''
+    return (2 * c + 1) / (2 * c)
+
+def calculate_cortante(k_s, F, D, d):
+    '''
+        Description
+        -----------
+        This function is used to calculate the cortante stress
+    '''
+    return k_s * ( 8 * F * D ) / ( np.pi * pow(d, 3) )
+
+
+def calculate_SUT(A, d, m):
+    '''
+        Description
+        -----------
+        This function is used to calculate the SUT        
+    '''
+    return A / pow(d, m)
+
