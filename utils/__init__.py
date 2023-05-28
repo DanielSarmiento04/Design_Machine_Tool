6174ad5f49bf9031f09aeb68cad3a9d000008d5c
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2023-05-27 19:32:02 -0500

    add aditional tables

--- utils/__init__.py
+++ utils/__init__.py
@@ -42,51 +42,5 @@ def calculate_k_f(d, units='usa')-> float:
     
     return 5.7 + 0.02682 * d
 
-import numpy as np
-
-
-'''
-    Resortes
-'''
-
-def calculate_indice_resorte(D:float, d:float):
-    '''
-        Description
-        -----------
-        This function is used to calculate the indice resorte
-    '''
-    return D / d
-
-
-def calcula_k_s(c):
-    '''
-        Description
-        -----------
-        This function is used to calculate the k_s
-    '''
-    return (2 * c + 1) / (2 * c)
-
-def calculate_cortante(k_s, F, D, d):
-    '''
-        Description
-        -----------
-        This function is used to calculate the cortante stress
-    '''
-    return k_s * ( 8 * F * D ) / ( np.pi * pow(d, 3) )
-
-
-def calculate_SUT(A, d, m):
-    '''
-        Description
-        -----------
-        This function is used to calculate the SUT        
-    '''
-    return A / pow(d, m)
-
-def calculate_deflection(F, D, na, d, G):
-    '''
-        Description
-        -----------
-        This function is used to calculate the deflection
-    '''
-    return (8 * F * pow(D, 3) * na) / (pow(d, 4) * G)
+def calculate_torque_subida(P, dp, µ, L):
+    pass
